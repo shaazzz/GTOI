f7fc025f0202f34bff42aef3fec4dbd1fba0ebbe
HKalbasi <kalbasi@ce.sharif.edu>
2020-03-06 00:55:56 +0330

    build problems in main build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,9 @@
 cd $(dirname "$0")
 cd ..
+rm -r text/_build/html
 cd text
-rm -r _build/html
 make html
-cp -r _build/html ../_build
+cd ..
+cp -r text/_build/html _build
+node scripts/problems.mjs
+cp -a problems/_build/. _build/problems
