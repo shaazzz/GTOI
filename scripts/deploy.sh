c17cf90696217d042e9a93802bb11a9b5b4a92b5
HKalbasi <kalbasi@ce.sharif.edu>
2020-03-08 18:59:00 +0330

    update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,2 @@
 cd $(dirname "$0")
-host=$(jq -r '.host' < deploy-config.json)
-path=$(jq -r '.path' < deploy-config.json)
-echo Enter remote password:
-read password
-cd ../text/_build/html
-7za a -r -t7z -m0=lzma2 -mx=9 -mfb=273 -md=28 -ms=8g -mmt=off -mmtf=off -mqs=on -bt -bb3 www.7z *
-sshpass -p $password scp www.7z "$host:/gtoi"
-SCRIPT="cd /gtoi; rm -r www; 7za x www.7z -owww; chmod 755 www"
-sshpass -p $password ssh $host "${SCRIPT}"
-rm www.7z
+node gh-pages.mjs
