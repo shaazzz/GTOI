d2bf2b8f04f59cc8a383225b7b70ac53598f53ed
hamidreza kalbasi <hamidrezakalbasi@protonmail.com>
2020-10-19 12:02:27 +0330

    add neato support

--- scripts/build-dot.sh
+++ scripts/build-dot.sh
@@ -4,3 +4,8 @@ do
   x="$(basename "$f" .dot)";
   dot -Tsvg $f -o "text/_static/dot/$x.svg"
 done
+for f in dot/*.neato;
+do
+  x="$(basename "$f" .neato)";
+  neato -Tsvg $f -o "text/_static/dot/$x.svg"
+done
